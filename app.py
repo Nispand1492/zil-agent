46c1aa349ca3495a6541c1e2e36a75c4460c28fc
nispandmehta <nispandmehta@microsoft.com>
2025-08-05 09:21:21 -0700

    test2

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, origins=["https://salmon-mud-01e8de810.1.azurestaticapps.net"])
 @app.route("/")
 def index():
     return "Hello from Azure!"
