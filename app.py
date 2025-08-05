ac9fb4aae0219437b7d32b34dfe357400ad5a6f6
Nispand1492 <Nispand1492@users.noreply.github.com>
2025-08-05 09:30:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=["https://salmon-mud-01e8de810.1.azurestaticapps.net"])
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return "Hello from Azure!"
+    return "Zil's LangChain Agent is running!"
